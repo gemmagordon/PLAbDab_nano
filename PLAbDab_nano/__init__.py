8291f96745d41e2597e6c8b65949095a9f313a14
gemmagordon <72207136+gemmagordon@users.noreply.github.com>
2024-07-02 09:38:40 +0100

    Update __init__.py

--- PLAbDab_nano/__init__.py
+++ PLAbDab_nano/__init__.py
@@ -1,1 +1,3 @@
+from PLAbDab_nano.database_search.sequence_search import SequenceSearch
+from PLAbDab_nano.database_search.structure_search import StructureSearch
 from PLAbDab_nano.database import PLAbDab_nano
